a6c795f3a1c6614e1990f5d11ea88b8383fd1dbf
Lyle-Stevenson <lyleandrewstevenson@gmail.com>
2018-06-07 04:53:51 +0100

    Comments to tree growth added.

--- LoggingGP/src/GeneticProgramming/Node.java
+++ LoggingGP/src/GeneticProgramming/Node.java
@@ -1,35 +1,49 @@
 package GeneticProgramming;
 
 public class Node {
-	   Object value;
-	   Node left, right;
-	   private Node parentNode = null;
-	   private int level;
+	Object value; // Value of function or termial node, i.e "+" or "1"
+	Node left, right; // References to this nodes children
+	private Node parentNode = null; // Reference to nodes parent.
+	private int level; // What level of depth this node is at within the tree.
 
-	   public Node(Object value)
-	   {
-	      this.value=value;
-	   }   
+	public Node(Object value) // Constructor
+	{
+		this.value = value;
+	}
 
-	   public Node(Object value, Node left, Node right) 
-	   {
-	      this.value = value;
-	      this.left = left;
-	      this.right = right;
-	   } 
+	public Node(Object value, Node left, Node right) {
+		this.value = value;
+		this.left = left;
+		this.right = right;
+	}
 
-	   // Getter & setter for the value.
-	   public Object getValue(){
-	      return value;}
-	   public void setValue(Object value){
-	      this.value = value;}
+	// Getter & setter for the value.
+	public Object getValue() {
+		return value;
+	}
 
-	   // Getters & setters for left & right nodes.
-	   public Node getLeft(){ return left;}
-	   public Node getRight(){ return right;}
-	   public void setLeft(Node ln){left = ln;}
-	   public void setRight(Node rn){right = rn;}
+	public void setValue(Object value) {
+		this.value = value;
+	}
 
+	// Getters & setters for left & right nodes.
+	public Node getLeft() {
+		return left;
+	}
+
+	public Node getRight() {
+		return right;
+	}
+
+	public void setLeft(Node ln) {
+		left = ln;
+	}
+
+	public void setRight(Node rn) {
+		right = rn;
+	}
+
+	// Getter & setter for the parent node.
 	public Node getParentNode() {
 		return parentNode;
 	}
@@ -38,6 +52,7 @@ public class Node {
 		this.parentNode = parentNode;
 	}
 
+	// Getter & setter for the level.
 	public int getLevel() {
 		return level;
 	}
@@ -45,5 +60,5 @@ public class Node {
 	public void setLevel(int level) {
 		this.level = level;
 	}
-	   
+
 }
--- LoggingGP/src/GeneticProgramming/Tree.java
+++ LoggingGP/src/GeneticProgramming/Tree.java
@@ -1,4 +1,5 @@
 package GeneticProgramming;
+
 import java.awt.List;
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -6,83 +7,84 @@ import java.util.Queue;
 import java.util.Random;
 import GeneticProgramming.Parameters;
 
-
 public class Tree {
 
-	 private static final String[] OPERATORS = {"+", "-", "/", "*"};
-	 private static final int MAX_OPERAND = 10;
+	private static final String[] OPERATORS = { "+", "-", "/", "*" }; // List of
+																		// possible
+																		// function
+																		// nodes
+	private static final int MAX_OPERAND = 10; // Possible terminal node values
 
-	 private static Random random = new Random();
-	 private static Parameters params = new Parameters();
-	 
-	 private Node head = null;
+	private static Random random = new Random();
+	private static Parameters params = new Parameters();
 
-//	    public static Tree full(int depth) {
-//	        if (depth > 1) {
-//	            String operator = OPERATORS[random.nextInt(OPERATORS.length)];
-//	            return new Tree(operator, full(depth - 1), full(depth - 1));
-//	        } else {
-//	            return new Tree(random.nextInt(MAX_OPERAND) + 1);
-//	        }
-//	    }
-	 
-	 	public Tree(){
-	 		this.head = generateHead();
-	 		this.head.setParentNode(null);
-	 		this.head.setLevel(0);
-	 		this.head = grow(this.head,params.maxDepth);
-	 	}
+	private Node head = null; // Reference to head of the tree
 
-	 	public Node generateHead(){
-	 		String operator = OPERATORS[random.nextInt(OPERATORS.length)];
-            return new Node(operator);
-	 	}
-	 	
-	 	
-	    public Node grow(Node parent, int depth) {
-	    	int level = parent.getLevel() +1;
-	    	for(int i = 0; i < 2; i ++){
-	    		Node child = null;
-		        if (level < depth && random.nextBoolean()) {
-		            String operator = OPERATORS[random.nextInt(OPERATORS.length)];
-		            child = new Node(operator);
-		            child.setLevel(parent.getLevel() +1);
-			        child.setParentNode(parent);
-			        if(i == 0){
-			        	parent.setLeft(child);
-			        }else{
-			        	parent.setRight(child);
-			        }
-			        child = grow(child, depth);
-		        } else {
-		            child = new Node(random.nextInt(MAX_OPERAND) + 1);
-		            child.setLevel(parent.getLevel() +1);
-			        child.setParentNode(parent);
-			        if(i == 0){
-			        	parent.setLeft(child);
-			        }else{
-			        	parent.setRight(child);
-			        }
-		        }	        
-	    	}
-	    	return parent;
-	    }
-	    
-	    public void printTree(){
-	    	 Queue<Node> q = new LinkedList<Node>();
-	    	 q.add(this.head);//You don't need to write the root here, it will be written in the loop
-	    	 while (!q.isEmpty())
-	    	 {
-	    	    Node n = q.poll();
-	    	    System.out.println(n.getValue() + " : Level - " + n.getLevel()); //Only write the value when you dequeue it
-	    	    if (n.left !=null)
-	    	    {
-	    	        q.add(n.left);//enqueue the left child
-	    	    }
-	    	    if (n.right !=null)
-	    	    {
-	    	       q.add(n.right);//enque the right child
-	    	    }
-	    	 }
-	    }
+	// public static Tree full(int depth) {
+	// if (depth > 1) {
+	// String operator = OPERATORS[random.nextInt(OPERATORS.length)];
+	// return new Tree(operator, full(depth - 1), full(depth - 1));
+	// } else {
+	// return new Tree(random.nextInt(MAX_OPERAND) + 1);
+	// }
+	// }
+
+	public Tree() { // Constructor
+		this.head = generateHead(); // Trees head is created on initialisation
+		this.head.setParentNode(null); // Head node has no parent
+		this.head.setLevel(0); // Sets level to 0
+		this.head = grow(this.head, params.maxDepth); // Call to generate the
+														// rest of tree.
+	}
+
+	public Node generateHead() { // Generates the head node of the tree
+		String operator = OPERATORS[random.nextInt(OPERATORS.length)]; // Selects a random fucntion for node.
+		return new Node(operator); // Returns head node
+	}
+
+	public Node grow(Node parent, int depth) { // Grows the tree from given
+												// node.
+		int level = parent.getLevel() + 1; // Level tracker to assign to newly
+											// created children.
+		for (int i = 0; i < 2; i++) { // Grow method is ran through twice as each node can have a left and right child
+			Node child = null; // Initialise the child
+			if (level < depth && random.nextBoolean()) { //50% chance the new child is a terminal or fucntion node.
+				String operator = OPERATORS[random.nextInt(OPERATORS.length)]; //Random function is selected
+				child = new Node(operator); //Child node is set
+				child.setLevel(parent.getLevel() + 1); //Childs level is set
+				child.setParentNode(parent); //Parent is set.
+				if (i == 0) { //If first iteration then its left child
+					parent.setLeft(child);
+				} else { //If second iteration then its right child
+					parent.setRight(child);
+				}
+				child = grow(child, depth); //Grow tree from this child
+			} else { //If child is selected to be a terminal node
+				child = new Node(random.nextInt(MAX_OPERAND) + 1); //Terminal value added to node
+				child.setLevel(parent.getLevel() + 1); //Childs level is set
+				child.setParentNode(parent);//Childs parent is set
+				if (i == 0) { //If first iteration then its left child
+					parent.setLeft(child);
+				} else { //If second iteration then its right child
+					parent.setRight(child);
+				}
+			}
+		}
+		return parent; //Returns head node once tree is complete.
+	}
+
+	public void printTree() {
+		Queue<Node> q = new LinkedList<Node>();
+		q.add(this.head);
+		while (!q.isEmpty()) {
+			Node n = q.poll();
+			System.out.println(n.getValue() + " : Level - " + n.getLevel()); 
+			if (n.left != null) {
+				q.add(n.left);// enqueue the left child
+			}
+			if (n.right != null) {
+				q.add(n.right);// enque the right child
+			}
+		}
+	}
 }
